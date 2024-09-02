c67e0a37f307fa75900b2ca07c77fc65e7f20308
Nikita <voitko1994@gmail.com>
2024-09-02 13:19:55 +0200

    and one more

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -86,19 +86,15 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 DATABASE_TYPE = os.getenv('DATABASE_TYPE', default='postgreSQL')
 DATABASE_URL = os.getenv('DATABASE_URL', default='DATABASE_URL')
 
-if DATABASE_TYPE == 'postgreSQL':
+if DATABASE_URL and 'postgres' in DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True
-        ),
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, conn_health_checks=True)
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
