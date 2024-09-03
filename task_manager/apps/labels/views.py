5e32022e74cd7c5f7ffab42c46c5a2dd1b581d12
Nikita <voitko1994@gmail.com>
2024-09-03 23:05:30 +0200

    labels flash

--- task_manager/apps/labels/views.py
+++ task_manager/apps/labels/views.py
@@ -20,12 +20,22 @@ class LabelCreateView(CreateView):
     template_name = 'labels/label_form.html'
     success_url = reverse_lazy('label-list')
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Метка успешно создана")
+        return response
+
 @method_decorator(login_required, name='dispatch')
 class LabelUpdateView(UpdateView):
     model = Label
     form_class = LabelForm
     template_name = 'labels/label_form.html'
     success_url = reverse_lazy('label-list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Метка успешно изменена")
+        return response
 
 @method_decorator(login_required, name='dispatch')
 class LabelDeleteView(DeleteView):
@@ -36,7 +46,7 @@ class LabelDeleteView(DeleteView):
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         if self.object.tasks_with_labels.exists():
-            messages.error(request, "Невозможно удалить метку, потому что она используется.")
+            messages.error(request, "Невозможно удалить метку, потому что она используется")
             return redirect(self.success_url)
-        messages.success(request, "Метка успешно удалена.")
+        messages.success(request, "Метка успешно удалена")
         return super().post(request, *args, **kwargs)
