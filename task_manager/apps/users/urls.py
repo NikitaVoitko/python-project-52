45067f97409d21f15ed53e31f568b0784390360e
Nikita <voitko1994@gmail.com>
2024-09-03 22:00:14 +0200

    url fix

--- task_manager/apps/users/urls.py
+++ task_manager/apps/users/urls.py
@@ -4,7 +4,7 @@ from .views import UserListView, UserCreateView, UserUpdateView, UserDeleteView,
 urlpatterns = [
     path('', UserListView.as_view(), name='user-list'),
     path('create/', UserCreateView.as_view(), name='user-create'),
-    path('<int:pk>/edit/', UserUpdateView.as_view(), name='user-update'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='user-update'),
     path('<int:pk>/delete/', UserDeleteView.as_view(), name='user-delete'),
 
 ]
